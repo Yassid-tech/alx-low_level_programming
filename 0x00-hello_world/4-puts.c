6a5221d86bff8982d0e9c17b4e4a4d5c64b80de2
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2023-10-05 12:15:32 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+/**
+ * print_message - Displays the message "Programming is like building a multilingual
+ *                puzzle, to the console.
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
