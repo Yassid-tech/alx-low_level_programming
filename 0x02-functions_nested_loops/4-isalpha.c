81498275b953d648402180fc71e3b54816dcd4c0
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2023-10-10 12:26:44 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - Checks if a character is alphabetic.
- *
+ * @c: the character to be checked
  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
