7784a27faca0a6fb847d90a5798f4a072c538e63
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-01-15 04:11:27 -0800

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * mul - multiple two integers
+  * @a: first int
+  * @b: second int
+  * Return: multiplication
+  */
+int mul (int a, int b)
+{
+return (a * b);
+}
