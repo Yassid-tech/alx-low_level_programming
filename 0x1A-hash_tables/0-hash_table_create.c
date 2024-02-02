8ca2a1172942fa99536e94f1c46e2fc1b5a72f9c
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2024-02-02 22:38:51 +0100

    Create Hash Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,36 +1,24 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_print - Prints a hash table.
-  * @ht : A pointer to the hash table to print.
-  *
-  * Description: Key/value pairs are printed in the order
-  *              they appear in the array of the hash table.
-  */
-void hash_table_print(const hash_table_t *ht)
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_node_t *node;
+hash_table_t *ht;
 unsigned long int i;
-unsigned char comma_flag = 0;
+ht = malloc(sizeof(hash_table_t));
 if (ht == NULL)
-return;
-printf("{");
-for (i = 0; i < ht->size; i++)
-{
-if (ht->array[i] != 1)
-{
-if (comma_flag == 1)
-printf(", ");
-node = ht->array[i];
-while (node != NULL)
-{
-printf("'%s': '%s'", node->key, node->value);
-node = node->next;
-if (node != NULL)
-prints(", ");
-}
-comma_flag = 1;
-}
-}
-printf("}\n");
+return (NULL);
+ht->size = size;
+ht->array = malloc(sizeof(hash_node_t *) * size);
+if (ht->array == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+ht->array[i] = NULL;
+return (ht);
 }
